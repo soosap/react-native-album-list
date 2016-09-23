63b154e17b0b4219967ea39bcdf700ee9b83525a
soosap <prasath.soosaithasan@gmail.com>
2016-09-23 22:48:14 +0200

    Register "GriderAlbums" component for iOS w/ react-native's AppRegistry

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => {
+	return (
+		<Text>Some Text</Text>
+	);
+};
+
+AppRegistry.registerComponent('GriderAlbums', () => App);
