d2b02488d5853e730ab56ddb4d5d9e46ec897efd
soosap <prasath.soosaithasan@gmail.com>
2016-09-24 11:39:35 +0200

    Expand the root component to take up the full content area of the device
    This is a react-native gotcha. Otherwise it appears as if you can scroll down to see the content of the app. But it will always bounce back to the top.

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-	<View>
+	<View style={{ flex: 1 }}>
 		<Header title="Albums" />
 		<AlbumList />
 	</View>
